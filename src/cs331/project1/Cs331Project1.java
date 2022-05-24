bdbf5b3e3477aed792e60a31e897e3d2c740361f
aehigdon1 <aehigdon1@gmail.com>
2022-05-24 10:05:37 -0500

    Here is the Project 1 for CS 350.

--- src/cs331/project1/Cs331Project1.java
+++ src/cs331/project1/Cs331Project1.java
@@ -1,8 +1,14 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+Problem Statement: PP 14 Write a program that computes and prints the mean and standard deviation of a list of integers 
+x1 through xn. Assume that there will be no more than 50 input values. Compute both the mean and the 
+standard deviation as floating point values.
+
+*/
+       
 package cs331.project1;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +20,21 @@ public class Cs331Project1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declare variables
+        double mean, sd;
+        int numOfInts;
+        boolean runAgain = true;
+        
+        Scanner inputDevice = new Scanner(System.in);
+        
+        //Ask user how many integers are in the list
+        System.out.println("This program will take a list of integers, then "
+                + "compute the mean and standard deviation for that list.");
+        System.out.println("How many integers are in your list?");
+        numOfInts = inputDevice.nextInt();
+        
+        //Prompt user for list of integers using a while loop
+        
     }
     
 }
