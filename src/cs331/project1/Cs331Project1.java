d438c45d1e254654d0fbab5b7d2c7cd3a90feaa9
Matthew Henderson <mhenderson7@stu.jsu.edu>
2022-05-23 22:14:31 -0500

    First commit

---
+++ src/cs331/project1/Cs331Project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cs331.project1;
+
+/**
+ *
+ * @author Matthew
+ */
+public class Cs331Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
