f6823bc21bfd58fa040f6a69e5904ebdfe3c2235
mdhendu12 <69565337+mdhendu12@users.noreply.github.com>
2022-05-24 12:03:17 -0500

    Work in progress

--- src/cs331/project1/Cs331Project1.java
+++ src/cs331/project1/Cs331Project1.java
@@ -8,17 +8,13 @@ standard deviation as floating point values.
        
 package cs331.project1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Matthew
- */
 public class Cs331Project1 {
+    
+    //private ArrayList<Integer> arr = new ArrayList<Integer>();
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Declare variables
         double mean, sd;
@@ -28,13 +24,25 @@ public class Cs331Project1 {
         Scanner inputDevice = new Scanner(System.in);
         
         //Ask user how many integers are in the list
-        System.out.println("This program will take a list of integers, then "
+       /* System.out.println("This program will take a list of integers, then "
                 + "compute the mean and standard deviation for that list.");
         System.out.println("How many integers are in your list?");
-        numOfInts = inputDevice.nextInt();
-        
+        numOfInts = inputDevice.nextInt(); */
+        ArrayList<Integer> arr = new ArrayList<Integer>();
         //Prompt user for list of integers using a while loop
-        
+        System.out.println("Please enter 50 or less integers seperated by spaces");
+        while(inputDevice.hasNext()) {
+            arr.add(inputDevice.nextInt());
+        }
+        System.out.println(computeMean(arr));
     }
     
+    public static double computeMean(ArrayList<Integer> arr) {
+        double sum = 0;
+        
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum / arr.size();
+    }
 }
